f606ce9bf5832c172331e3183df92060e2405fb4
Dzianis_Pikus <denis.pikus@gmail.com>
2022-03-09 11:43:47 +0300

    Lesson 3 IoC & DI

--- src/main/java/com/dpdev/spring/service/UserService.java
+++ src/main/java/com/dpdev/spring/service/UserService.java
@@ -1,4 +1,15 @@
 package com.dpdev.spring.service;
 
+import database.repository.CompanyRepository;
+import database.repository.UserRepository;
+
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
+
+    public UserService(UserRepository userRepository, CompanyRepository companyRepository) {
+        this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
+    }
 }
--- src/main/java/database/repository/UserRepository.java
+++ src/main/java/database/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package database.repository;
 
+import database.pool.ConnectionPool;
+
 public class UserRepository {
+
+    private final ConnectionPool connectionPool;
+
+    public UserRepository(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
 }
