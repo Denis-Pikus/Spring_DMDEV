3c86537310617e32b5146892a9f45d7c3df814b3
Dzianis_Pikus <denis.pikus@gmail.com>
2022-03-25 10:24:47 +0300

    Lesson 29 EventListener

--- src/main/java/com/dmdev/spring/service/CompanyService.java
+++ src/main/java/com/dmdev/spring/service/CompanyService.java
@@ -1,13 +1,36 @@
 package com.dmdev.spring.service;
 
+import java.util.Optional;
+
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import com.dmdev.spring.database.entity.Company;
+import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.dto.CompanyReadDto;
+import com.dmdev.spring.listener.entity.AccessType;
+import com.dmdev.spring.listener.entity.EntityEvent;
 
 @Service
 public class CompanyService {
 
+    private final CrudRepository<Integer, Company> companyRepository;
     private final UserService userService;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public CompanyService(UserService userService) {
+    public CompanyService(
+        CrudRepository<Integer, Company> companyRepository,
+        UserService userService, ApplicationEventPublisher eventPublisher) {
+        this.companyRepository = companyRepository;
         this.userService = userService;
+        this.eventPublisher = eventPublisher;
+    }
+
+    public Optional<CompanyReadDto> findById(Integer id) {
+        return companyRepository.findById(id)
+            .map(entity -> {
+                eventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
+                return new CompanyReadDto(entity.id());
+            });
     }
 }
